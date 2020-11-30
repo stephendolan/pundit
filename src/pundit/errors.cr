6726dd928b11d94de2b7cc1ca44fe231019cc0bf
Stephen Dolan <dolan.stephen1@gmail.com>
2020-11-30 17:46:42 -0500

    Remove strict Lucky dependency

--- src/pundit/errors.cr
+++ src/pundit/errors.cr
@@ -3,18 +3,8 @@ module Pundit
   end
 
   class NotAuthorizedError < Error
-    include Lucky::RenderableError
-
     def initialize
       super("Action not permitted")
-    end
-
-    def renderable_status : Int32
-      401
-    end
-
-    def renderable_message : String
-      "You're not allowed to access that action for that resource."
     end
   end
 end
